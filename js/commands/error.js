be953293019b0cebf443df1bcf31c6a2e7f9d92b
lachiu <mathieu.van.daele@hotmail.com>
2023-06-08 16:49:48 +0200

    init v0.3

--- js/commands/error.js
+++ js/commands/error.js
@@ -1,14 +1,8 @@
 function show_error_output(input) {
     result=generate_element({elementname: "div", classname: "result"});
     tmp=generate_element({elementname: "span", classname: "terminal_line", innerHTML: `bash: ${input}: <span style=color:orange;>command</span> not found`});
-    // terminal_line=document.createElement("span");
-    // terminal_line.className="terminal_line";
-    // terminal_line.innerHTML=`bash: ${input}: <span style=color:orange;>command</span> not found`;
     result.appendChild(tmp);
     tmp=generate_element({elementname: "span", classname: "terminal_line", innerHTML: `Type <span style=color:orange;>help</span> to get a list of all allowed commands`});
-    // terminal_line=document.createElement("span");
-    // terminal_line.className="terminal_line";
-    // terminal_line.innerHTML=`Type <span style=color:orange;>help</span> to get a list of all allowed commands`;
     result.appendChild(tmp);
     return result;
 }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-terminal_prompt_command=document.getElementById("terminal_prompt_command");
-const accepted_commands=["ls", "exit", "help"];
+const terminal_prompt_command=document.getElementById("terminal_prompt_command");
+const accepted_commands=["cd", "ls", "lynx", "help", "exit"];
 const history=[];
+let current_directory="~";
+let files=null;
 
 function handle_on_load() {
     body=document.getElementsByTagName("body");
@@ -11,21 +13,38 @@ function handle_on_load() {
         element=generate_element({elementname: "script", newattr: "src", newvalue: filename});
         scripts.appendChild(element);
     }
+
+    fetch("http://portfolio.localhost/assets/json/files.json")
+    .then(response => response.json())
+    .then(data => {
+        files = data;
+    });
+
+    terminal_prompt_command.value="This is a test";
 }
 
-function handle_on_input(elem) {
-    elem.target.style.width = elem.target.value.length + "ch";
+function handle_on_input(event) {
+    console.log(event);
+    if (event.code.includes("Key") || event.code.includes("Digit") || event.code.includes("Numpad") || event.code == "Space") {
+        event.currentTarget.style.width = (event.currentTarget.value.length + 1) + "ch";
+    } else if (event.code == "Backspace" || event.code == "Delete") {
+        event.currentTarget.style.width = (event.currentTarget.value.length - 1) + "ch";
+    }
 }
 
 function is_valid_command(input) {
+    input=input[0];
     valid=false;
-    for (let index = 0; index < accepted_commands.length; index++) {
-        const element = accepted_commands[index];
-        if (element.includes(input)) {
-            valid=true;
+    if (typeof input == "string" && input.length>0) {
+        for (let index = 0; index < accepted_commands.length; index++) {
+            const element = accepted_commands[index];
+            console.log(`element: ${element} - ${element.includes(input)}`);
+            if (element.includes(input)) {
+                valid=true;
+            }
         }
     }
-
+    console.log(valid);
     return valid;
 }
 
@@ -34,7 +53,7 @@ function reset_input() {
     terminal_prompt_command.style.width="0ch";
 }
 
-// {elementname: "", classname: "", value: "", textcontent: "", newattr: "", newvalue: ""}
+// {elementname: "", classname: "", value: "", innerHTML: "", textcontent: "", newattr: "", newvalue: ""}
 function generate_element(parameters) {
     tmp=null;
     if (typeof parameters.elementname !== "undefined") {
@@ -50,9 +69,9 @@ function generate_element(parameters) {
 
 function handle_command(input) {
     result=null;
-    valid=is_valid_command(input);
-    if (valid) {
-        input_array=input.split(" ");
+    input_array=input.split(" ");
+    valid=is_valid_command(input_array);
+    if (valid) {        
         result=window["show_" + input_array[0] + "_output"](input_array);
         //result="show_" + input_array[0] + "_output"();
         // switch (input_array[0]) {
@@ -79,50 +98,41 @@ function handle_command(input) {
 }
 
 function handle_on_submit(event) {
-    username=document.getElementById("terminal_prompt_username").textContent;
-    user_host_seperator=document.getElementById("terminal_prompt_username_hostname_seperator").textContent;
-    hostname=document.getElementById("terminal_prompt_hostname").textContent;
-    seperator=document.getElementById("terminal_prompt_seperator").textContent;
-    path=document.getElementById("active_terminal_prompt_path").textContent;
-    symbol=document.getElementById("active_terminal_prompt_symbol").textContent;
     input=terminal_prompt_command.value;
+    console.log(`Input: ${typeof input} - ${input} - ${input.length}`);
+    if (typeof input == "string" && input.length>0) {
+        username=document.getElementById("terminal_prompt_username").textContent;
+        user_host_seperator=document.getElementById("terminal_prompt_username_hostname_seperator").textContent;
+        hostname=document.getElementById("terminal_prompt_hostname").textContent;
+        seperator=document.getElementById("terminal_prompt_seperator").textContent;
+        path=document.getElementById("active_terminal_prompt_path").textContent;
+        symbol=document.getElementById("active_terminal_prompt_symbol").textContent;
+        
 
-    terminal_prompt_line=generate_element({elementname: "div", classname: "terminal_prompt_line"});
-    // terminal_prompt_line=document.createElement("div");
-    // terminal_prompt_line.className="terminal_prompt_line";
-    terminal_prompt_history=generate_element({elementname: "span", classname: "terminal_prompt_history", textcontent: `${username}${user_host_seperator}${hostname}${seperator}`});
-    // terminal_prompt_history=document.createElement("span");
-    // terminal_prompt_history.textContent=`${username}${user_host_seperator}${hostname}${seperator}`;
-    // terminal_prompt_history.className="terminal_prompt_history";
-    terminal_prompt_path=generate_element({elementname: "span", classname: "terminal_prompt_path", textcontent: path});
-    // terminal_prompt_path=document.createElement("span");
-    // terminal_prompt_path.textContent=path;
-    // terminal_prompt_path.className="terminal_prompt_path";
-    terminal_prompt_symbol=generate_element({elementname: "span", classname: "terminal_prompt_symbol", textcontent: symbol});
-    // terminal_prompt_symbol=document.createElement("span");
-    // terminal_prompt_symbol.textContent=symbol;
-    // terminal_prompt_symbol.className="terminal_prompt_symbol";
-    
-    old_command=generate_element({elementname: "span", classname: "old_command", textcontent: input});
-    // old_command=document.createElement("span");
-    // old_command.textContent=input;
-    // old_command.className="old_command";
+        terminal_prompt_line=generate_element({elementname: "div", classname: "terminal_prompt_line"});
+        terminal_prompt_history=generate_element({elementname: "span", classname: "terminal_prompt_history", textcontent: `${username}${user_host_seperator}${hostname}${seperator}`});
+        terminal_prompt_path=generate_element({elementname: "span", classname: "terminal_prompt_path", textcontent: path});
+        terminal_prompt_symbol=generate_element({elementname: "span", classname: "terminal_prompt_symbol", textcontent: symbol});    
+        old_command=generate_element({elementname: "span", classname: "old_command", textcontent: input});
 
-    result=handle_command(input);    
+        result=handle_command(input);
 
-    terminal_prompt_line.appendChild(terminal_prompt_history);
-    terminal_prompt_line.appendChild(terminal_prompt_path);
-    terminal_prompt_line.appendChild(terminal_prompt_symbol);
-    terminal_prompt_line.appendChild(old_command);
-    terminal_prompt_line.appendChild(result);
+        terminal_prompt_line.appendChild(terminal_prompt_history);
+        terminal_prompt_line.appendChild(terminal_prompt_path);
+        terminal_prompt_line.appendChild(terminal_prompt_symbol);
+        terminal_prompt_line.appendChild(old_command);
+        if (typeof result !== "undefined" && result !== null) { terminal_prompt_line.appendChild(result); }
 
-    parent_div=document.getElementById("terminal_commands");
-    parent_div.insertBefore(terminal_prompt_line, document.getElementById("active_line"));
-    reset_input();
+        parent_div=document.getElementById("terminal_commands");
+        parent_div.insertBefore(terminal_prompt_line, document.getElementById("active_line"));
+        reset_input();
+        parent_div.scrollTop=parent_div.scrollHeight;
+    }
     event.preventDefault();
+    return false;
 }
 
 window.addEventListener("load",handle_on_load);
-document.addEventListener("keypress", function() { terminal_prompt_command.focus() });
-terminal_prompt_command.addEventListener("input",handle_on_input.bind(this));
+document.addEventListener("keydown", function() { terminal_prompt_command.focus() });
+terminal_prompt_command.addEventListener("keydown",handle_on_input.bind(this));
 document.getElementById("active_command").addEventListener("submit",handle_on_submit.bind(this));
