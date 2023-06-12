42e8c6542eb2bd00108cffe68df180cd0c5a04b4
lachiu <mathieu.van.daele@hotmail.com>
2023-06-12 23:31:27 +0200

    Added help command

---
+++ js/commands/help.js
@@ -0,0 +1,6 @@
+function show_ls_output($args) {
+    result=generate_element({elementname: "div", classname: "result"});
+    line_one=`Er zijn slechts een aantal commands beschikbaar, deze zijn: ls, history, help, mozilla, exit`;
+    result.appendChild(generate_element({elementname: "span", classname: "terminal_line", textcontent: line_one}));
+    return result;
+}
