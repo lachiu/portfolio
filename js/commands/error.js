3a55c64b6b1bf3337ee76a13c0f2937b1c94489d
lachiu <mathieu.van.daele@hotmail.com>
2023-06-12 23:31:11 +0200

    Changed error message to dutch

--- js/commands/error.js
+++ js/commands/error.js
@@ -1,8 +1,8 @@
 function show_error_output(input) {
     result=generate_element({elementname: "div", classname: "result"});
-    tmp=generate_element({elementname: "span", classname: "terminal_line", innerHTML: `bash: ${input}: <span style=color:orange;>command</span> not found`});
+    tmp=generate_element({elementname: "span", classname: "terminal_line", innerHTML: `bash: ${input}: <span style=color:orange;>command</span> bestaat niet`});
     result.appendChild(tmp);
-    tmp=generate_element({elementname: "span", classname: "terminal_line", innerHTML: `Type <span style=color:orange;>help</span> to get a list of all allowed commands`});
+    tmp=generate_element({elementname: "span", classname: "terminal_line", innerHTML: `Type <span style=color:orange;>help</span> om een lijst te krijgen met beschikbare commando's.`});
     result.appendChild(tmp);
     return result;
 }
