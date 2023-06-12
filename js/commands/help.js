0bffdf8f46f4a80c56af1d35d220fb12cd5304b8
lachiu <mathieu.van.daele@hotmail.com>
2023-06-12 23:37:05 +0200

    Added color

--- js/commands/help.js
+++ js/commands/help.js
@@ -1,6 +1,6 @@
 function show_help_output($args) {
     result=generate_element({elementname: "div", classname: "result"});
-    line_one=`Er zijn slechts een aantal commands beschikbaar, deze zijn: ls, history, help, mozilla, exit`;
-    result.appendChild(generate_element({elementname: "span", classname: "terminal_line", textcontent: line_one}));
-    return result;
+    line_one=`Er zijn slechts een aantal commands beschikbaar, deze zijn: <span style=color:orange;>ls</span>, <span style=color:orange;>history</span>, <span style=color:orange;>help</span>, <span style=color:orange;>mozilla</span>, <span style=color:orange;>exit</span>`;
+    result.appendChild(generate_element({elementname: "span", classname: "terminal_line", innerHTML: line_one}));
+    return result;    
 }
