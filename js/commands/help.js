8d1333e65c5e8ddd1665b5a9e5626829c0d4f637
lachiu <mathieu.van.daele@hotmail.com>
2023-06-12 23:35:54 +0200

    Fixed big typo

--- js/commands/help.js
+++ js/commands/help.js
@@ -1,4 +1,4 @@
-function show_ls_output($args) {
+function show_help_output($args) {
     result=generate_element({elementname: "div", classname: "result"});
     line_one=`Er zijn slechts een aantal commands beschikbaar, deze zijn: ls, history, help, mozilla, exit`;
     result.appendChild(generate_element({elementname: "span", classname: "terminal_line", textcontent: line_one}));
