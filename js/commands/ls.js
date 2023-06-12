3e9850f24c298f0fe5491f62f6915f9481e93d71
lachiu <mathieu.van.daele@hotmail.com>
2023-06-12 23:32:23 +0200

    Changed formatting

--- js/commands/ls.js
+++ js/commands/ls.js
@@ -1,11 +1,30 @@
-function show_ls_output($args) {    
-    result=generate_element({elementname: "div", classname: "result"});
-    for (let index = 0; index < 6; index++) {
-        tmp=generate_element({elementname: "span", classname: "terminal_line", innerHTML: `-rw-------   1 www-data        www-data        99999 Jan 20 10:00 file.txt`, newattr: "style", newvalue: "white-space-collapse: preserve;"});
-        // terminal_line=document.createElement("span");
-        // terminal_line.className="terminal_line";
-        // terminal_line.textContent=;
-        result.appendChild(tmp);
+function show_ls_output($args) {
+    function make_format(requiredsize, size) {
+        tmp=size.toString();
+        required_size=requiredsize-tmp.length;
+        string="";
+        while (string.length<required_size) {
+            string+=" ";
+        }
+        string+=tmp;
+        return string;
     }
+
+    result=generate_element({elementname: "div", classname: "result"});    
+    tmp=files[current_directory].files;
+    console.log(tmp.length);
+    if (typeof tmp !== "undefined") {
+        tmp.forEach(selected_file => {
+            permission=selected_file.permission;
+            number=make_format(4,"1");
+            filesize=make_format(13,selected_file.size);
+            group=make_format(20,selected_file.group);
+            span=generate_element({elementname: "span", classname: "terminal_line", innerHTML: `${permission}${number} ${selected_file.user}${group}${filesize} ${selected_file.date} ${selected_file.time} ${selected_file.name}`, newattr: "style", newvalue: "white-space-collapse: preserve;"});
+            result.appendChild(span);
+        });
+    } else {
+        span=generate_element({elementname: "span", classname: "terminal_line", innerHTML: `Directory <span style=color:orange;>${folder}</span> not found`});        
+        result.appendChild(span);
+    }    
     return result;
 }
